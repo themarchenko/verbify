5c6fdd05094822a7ecadf1f09d6391b247fac824
Leonid Marchenko <dev.leonid.marchenko@gmail.com>
2026-04-13 11:21:53 +0200

    Add Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { getLocale, getMessages } from 'next-intl/server'
 import { Geist, Geist_Mono } from 'next/font/google'
 
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Toaster } from 'sonner'
 
 import './globals.css'
@@ -39,6 +40,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <NextIntlClientProvider messages={messages}>
           {children}
           <Analytics />
+          <SpeedInsights />
           <Toaster position="bottom-right" />
         </NextIntlClientProvider>
       </body>
